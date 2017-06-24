ada462f1d0fe7e2d76af0be57277f7d70683a355
Safeeul Bashir Safee <safee_cuet06@yahoo.com>
2017-06-24 14:24:24 -0500

    [Change Type] Add Employee feature added

--- src/main/java/com/safee/DAO/EmployeeDao.java
+++ src/main/java/com/safee/DAO/EmployeeDao.java
@@ -1,15 +1,15 @@
 package com.safee.DAO;
 
-import java.util.Date;
 import java.util.List;
 
 import javax.transaction.Transactional;
 
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.orm.hibernate4.HibernateTemplate;
 import org.springframework.stereotype.Repository;
 
 import com.safee.DAO.interfaces.EmployeeDaoInterface;
@@ -20,54 +20,38 @@ import com.safee.model.Employee;
 public class EmployeeDao implements EmployeeDaoInterface {
 	private static final Logger logger = LoggerFactory.getLogger(EmployeeDao.class);
 	@Autowired
-	private HibernateTemplate hibernateTemplate;
-
-	@SuppressWarnings("unchecked")
-	@Override
-	public List<Employee> getAllEmployee() {
-		String hql = "FROM Employee as e ORDER BY p.first_name";
-		return (List<Employee>) hibernateTemplate.find(hql);
-	}
+	private SessionFactory sessionFactory;
 
 	@Override
 	@Transactional
 	public void addEmployee(Employee employee) {
 		logger.debug(employee.getFirstName());
-		hibernateTemplate.save(employee);
+		Session session = sessionFactory.openSession();
+		Transaction transaction = session.beginTransaction();
+		session.save(employee);
+		transaction.commit();
 		// hibernateTemplate.save(employee);
 	}
 
 	@Override
 	public Employee getEmployee(Integer employeeNo) {
-		logger.debug("Employee ID: " + employeeNo);
-		if (hibernateTemplate == null)
-			logger.debug("template is null");
-
-		return hibernateTemplate.get(Employee.class, employeeNo);
-
+		/*
+		 * logger.debug("Employee ID: " + employeeNo); if (hibernateTemplate ==
+		 * null) logger.debug("template is null");
+		 * 
+		 * return hibernateTemplate.get(Employee.class, employeeNo);
+		 */return null;
 	}
 
 	@Override
 	public void updateEmployee(Employee employee) {
 
+	}
+
+	@Override
+	public List<Employee> getAllEmployee() {
 		// TODO Auto-generated method stub
-		/*
-		 * try (Connection connection = JdbcConnectionFactory.getConnection();)
-		 * { String SQL =
-		 * "UPDATE employees SET first_name=?, last_name=?, birth_date=?, hire_date=? where emp_no=?"
-		 * ; PreparedStatement preparedStatement =
-		 * connection.prepareStatement(SQL); preparedStatement.setString(1,
-		 * employee.getFirstName()); preparedStatement.setString(2,
-		 * employee.getLastName()); preparedStatement.setDate(3, new
-		 * java.sql.Date(employee.getBirthDate().getTime()));
-		 * preparedStatement.setDate(4, new
-		 * java.sql.Date(employee.getHireDate().getTime()));
-		 * preparedStatement.setLong(5, employee.getEmployeeNo());
-		 * logger.debug(preparedStatement.toString());
-		 * preparedStatement.executeUpdate();
-		 * 
-		 * } catch (SQLException e) { // TODO Auto-generated catch block
-		 * e.printStackTrace(); }
-		 */ }
+		return null;
+	}
 
 }
--- src/main/java/com/safee/SpringBootWebApplication.java
+++ src/main/java/com/safee/SpringBootWebApplication.java
@@ -1,13 +1,20 @@
 package com.safee;
 
+import javax.persistence.EntityManagerFactory;
+
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringBootWebApplication extends SpringBootServletInitializer {
 
+	@Autowired
+	private EntityManagerFactory entityManagerFactory;
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 		return application.sources(SpringBootWebApplication.class);
@@ -16,5 +23,12 @@ public class SpringBootWebApplication extends SpringBootServletInitializer {
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(SpringBootWebApplication.class, args);
 	}
+	@Bean
+	public SessionFactory getSessionFactory() {
+	    if (entityManagerFactory.unwrap(SessionFactory.class) == null) {
+	        throw new NullPointerException("factory is not a hibernate factory");
+	    }
+	    return entityManagerFactory.unwrap(SessionFactory.class);
+	}	
 
 }
